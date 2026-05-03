6f625a75dc7a1f6434a6c72e11dedc3fc1ed9448
hwanggyuun <gka365@naver.com>
2026-05-03 19:26:40 +0900

    [feat] Add update method to WeeklyReport and new query for finding reports by user and start date

--- src/main/java/com/afternote/domain/mindrecord/weekly/repository/WeeklyReportRepository.java
+++ src/main/java/com/afternote/domain/mindrecord/weekly/repository/WeeklyReportRepository.java
@@ -4,10 +4,14 @@ import com.afternote.domain.mindrecord.weekly.model.WeeklyReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WeeklyReportRepository extends JpaRepository<WeeklyReport, Long> {
 
     List<WeeklyReport> findByUserIdOrderByEndDateDesc(Long userId);
+
+    Optional<WeeklyReport> findByUserIdAndStartDate(Long userId, LocalDateTime startDate);
 }

9269dfa7e29577f2eec7650ac4579cbf47779b13
hwanggyuun <gka365@naver.com>
2026-06-20 21:50:18 +0900

    fix : error

---
+++ src/main/java/com/afternote/domain/timeletter/repository/TimeLetterMediaRepository.java
@@ -0,0 +1,19 @@
+package com.afternote.domain.timeletter.repository;
+
+import com.afternote.domain.timeletter.model.TimeLetterMedia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TimeLetterMediaRepository extends JpaRepository<TimeLetterMedia, Long> {
+
+    List<TimeLetterMedia> findByTimeLetterId(Long timeLetterId);
+
+    List<TimeLetterMedia> findByTimeLetterIdIn(List<Long> timeLetterIds);
+
+    void deleteByTimeLetterId(Long timeLetterId);
+
+    void deleteByTimeLetterIdIn(List<Long> timeLetterIds);
+}

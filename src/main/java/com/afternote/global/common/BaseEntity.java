137be9b3878dbb63d6318e1c79b063570d96e34b
hwanggyuun <gka365@naver.com>
2026-07-27 13:53:45 +0900

    refactory: DB-auto

--- src/main/java/com/afternote/global/common/BaseEntity.java
+++ src/main/java/com/afternote/global/common/BaseEntity.java
@@ -10,10 +10,22 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public abstract class BaseEntity {
 
-    @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
+    @Column(
+            name = "created_at",
+            nullable = false,
+            insertable = false,
+            updatable = false,
+            columnDefinition = "DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)"
+    )
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at", nullable = false, insertable = false, updatable = false)
+    @Column(
+            name = "updated_at",
+            nullable = false,
+            insertable = false,
+            updatable = false,
+            columnDefinition = "DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)"
+    )
     private LocalDateTime updatedAt;
 
 }
